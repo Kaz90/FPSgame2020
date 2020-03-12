317ba4e67a3b8082d26dbe45551c25e4d1988a83
Alex M <33159385+AlexM5150@users.noreply.github.com>
2020-03-12 14:54:04 -0700

    bullet projectiles with damage

---
+++ TheGame/AnFpsGame/Assets/Scripts/bulletBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletBehavior : MonoBehaviour
+{
+    public float damage = 10f;
+    private void OnTriggerEnter(Collider other)
+    
+    {
+        print("hit " + other.name + '!');
+        Destroy(gameObject);
+        Target target = other.transform.GetComponent<Target>();
+        if (target != null)
+        {
+            target.TakeDamage(damage);
+        }
+    }
+}
